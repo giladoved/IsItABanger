ccaeef320c44682fdb4c270ce2093ca99c87535a
Gilad Oved <giladoved@gmail.com>
2017-04-15 23:05:34 -0500

    fixes audio

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 var input = document.getElementById("song");
 var result = document.getElementById("result");
-var audio;
 var preview_url;
 
 $(function(){
+	var audio = new Audio();
+    audio.pause();
+    audio.currentTime = 0;
+    audio.src = '';
+
     $("#form").on("submit", function(e){
-   		audio = new Audio();
         query = $("#song").val();
 		$.ajax({
 	        url: 'https://api.spotify.com/v1/search',
@@ -19,21 +22,21 @@ $(function(){
 				var identifier = response["tracks"]["items"][0]["id"];
 				preview_url = response["tracks"]["items"][0]["preview_url"];
 				console.log(identifier);
-				decider(identifier);
+				decider(identifier, audio);
 	        },
 	        error: function(err) {
-	        	console.log("err:" + err);
+	        	console.log("err:" + JSON.stringify(err));
 	        }
 	    });
         e.preventDefault();
     });
 
-	function decider(id){
+	function decider(id, audio){
 		$.ajax({
 			url: 'https://api.spotify.com/v1/audio-features/' + id,
 			type: 'GET',
 			beforeSend: function (xhr) {
-			    xhr.setRequestHeader('Authorization', 'Bearer BQAqh7qSv7rnIk026Kj5Utn08CxAliE0iFcf-AfBrj8FnTtXjOJRZ80pQR3Qj55NuUcflR3dbgNC83R4E1M1b_BsytPXeDT-GGzjTLY9lFt3TfKKXPJLjdZJG8b-JrvZyGSVsgOyrw5ey9p5QSBDfZqXEyv7PAbzjc8JLgS7j--xJFKDvhwd-jEuh-zzIvw');
+			    xhr.setRequestHeader('Authorization', 'Bearer BQBto9CGr-FsvzLTqcIx41eXH-D3cttb1_lKsclEIXm4POqhx8cSdTQQIbxkQbA3wu1eYC6TzPbvCCG18_p2iIZjRQX7VP5areAGSNdDnVc2YZrvL6d0cMl35MZUp69YAToKSy4Y8QK1HfcFariBbf3x6arOuaj0m7cY3zNnVy5v4eTrV01JC5kh-s0P1T0');
 			},
 			success: function(data) {
 		        console.log('success');
@@ -46,13 +49,15 @@ $(function(){
 		        console.log('danceability: ' + danceability);
 
 		        if (energy && danceability && (energy+danceability <= 1.27)) {
-		        	console.log('not hype')
+		        	console.log('not hype');
+		        	result.innerHTML = "Not hype. Your music sucks.";
 		        	return;
 		        }
 
 		        var yes_answers = ["Is the sky blue?", "Does a bear shit in the woods?", "Is the Pope catholic?", "Are the Kennedy's gun shy?", "Are there naked dicks in gay porn?", "Does OU Suck?", "Duh.", "Of course.. did you really just ask me that?", "No shit.. that's a dumbass question.", "Yes", "yes...", "Yes."];
 				var rand_yes = Math.floor((Math.random() * yes_answers.length-1));
 				console.log(yes_answers[rand_yes]);
+				result.innerHTML = yes_answers[rand_yes];
 
 		        console.log("it's hype!!!");
 		        audio.src = preview_url;
